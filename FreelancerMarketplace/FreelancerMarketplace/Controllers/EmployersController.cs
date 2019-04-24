82d2f9f8d3790c638ad9b167e0accaf29109db19
rida0910 <43719846+rida0910@users.noreply.github.com>
2019-04-24 19:39:18 +0500

    Post a job view

---
+++ FreelancerMarketplace/FreelancerMarketplace/Controllers/EmployersController.cs
@@ -0,0 +1,24 @@
+﻿using FreelancerMarketplace.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreelancerMarketplace.Controllers
+{
+    public class EmployersController : Controller
+    {
+        private DB66Entities db = new DB66Entities();
+        // GET: Employers
+        public ActionResult POstAJOb()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
+            ViewBag.JobType = new SelectList(db.Lookups.Where(x => x.category.Equals("JOBTYPE")), "Id", "value");
+            return View();
+        }
+
+
+
+    }
+}
