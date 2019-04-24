e824f7ff15ba14ef4051ccb980c75bae022b42b0
rida0910 <43719846+rida0910@users.noreply.github.com>
2019-04-25 01:11:19 +0500

    Post a job backend completed

--- FreelancerMarketplace/FreelancerMarketplace/Controllers/EmployersController.cs
+++ FreelancerMarketplace/FreelancerMarketplace/Controllers/EmployersController.cs
@@ -1,6 +1,8 @@
 ﻿using FreelancerMarketplace.Models;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,87 @@ namespace FreelancerMarketplace.Controllers
             ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
             ViewBag.JobType = new SelectList(db.Lookups.Where(x => x.category.Equals("JOBTYPE")), "Id", "value");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult POstAJOb([Bind(Include = "Title,Description,MinPayment,MaxPayment,AttachmentFile,CategoryId,Deadline,JobType")] Job job,
+            string MainSkill, string OtherSkills)
+        {
+            if (ModelState.IsValid)
+            {
+                Attachment attachment = new Attachment();
+                string fileName = Path.GetFileNameWithoutExtension(job.AttachmentFile.FileName);
+                string extension = Path.GetExtension(job.AttachmentFile.FileName);
+                fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                attachment.Path = "~/Images/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/Images/"), fileName);
+                job.AttachmentFile.SaveAs(fileName);
+
+                db.Attachments.Add(attachment);
+                db.SaveChanges();
+
+                job.AttachmentID = attachment.AttachmentId;
+                Skill skill = new Skill();
+                if (!db.Skills.Any(x => x.SkillName == MainSkill))
+                {
+
+                    skill.SkillName = MainSkill;
+                    db.Skills.Add(skill);
+                    db.SaveChanges();
+
+                    job.SkillID = skill.SkillId;
+                }
+                else
+                {
+                    job.SkillID = db.Skills.FirstOrDefault(x => x.SkillName.Equals(MainSkill)).SkillId;
+                }
+                var id = User.Identity.GetUserId();
+                job.EmployerID = db.People.FirstOrDefault(p => p.User_AccountID.Equals(id)).PersonId;
+                db.Jobs.Add(job);
+                db.SaveChanges();
+
+                var SkillsList = OtherSkills.Split(',');
+                foreach (string s in SkillsList)
+                {
+                    Skill skill1 = new Skill();
+                    if (!db.Skills.Any(x => x.SkillName == s))
+                    {
+
+                        skill1.SkillName = s;
+                        db.Skills.Add(skill1);
+                        db.SaveChanges();
+
+                        if (!db.JobSkills.Any(x => x.Skill.SkillName == s))
+                        {
+                            JobSkill jobSkill = new JobSkill();
+                            jobSkill.Skill = skill;
+                            jobSkill.SkillId = skill.SkillId;
+                            jobSkill.JobId = job.JobId;
+                            db.JobSkills.Add(jobSkill);
+                            db.SaveChanges();
+                        }
+                    }
+                    else
+                    {
+                        if (!db.FreelancerSkills.Any(x => x.Skill.SkillName == s))
+                        {
+                            JobSkill jobSkill = new JobSkill();
+                            jobSkill.SkillId = db.Skills.FirstOrDefault(x => x.SkillName.Equals(s)).SkillId;
+                            jobSkill.JobId = job.JobId;
+                            db.JobSkills.Add(jobSkill);
+                            db.SaveChanges();
+                        }
+                    }
+                }
+
+                ModelState.Clear();
+                return RedirectToAction("Dashboard");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
+            ViewBag.JobType = new SelectList(db.Lookups.Where(x => x.category.Equals("JOBTYPE")), "Id", "value");
+            return View(job);
         }
 
 
--- FreelancerMarketplace/FreelancerMarketplace/Models/Job.cs
+++ FreelancerMarketplace/FreelancerMarketplace/Models/Job.cs
@@ -11,18 +11,24 @@ namespace FreelancerMarketplace.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+
     public partial class Job
     {
         public int JobId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public Nullable<int> AttachmentID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public System.DateTime Expiry { get; set; }
         public int SkillID { get; set; }
         public int EmployerID { get; set; }
         public System.DateTime TimePosted { get; set; }
         public int CategoryID { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public Nullable<System.DateTime> Deadline { get; set; }
         public int MinPayment { get; set; }
         public Nullable<int> JobType { get; set; }
@@ -32,5 +38,7 @@ namespace FreelancerMarketplace.Models
         public virtual Category Category { get; set; }
         public virtual Skill Skill { get; set; }
         public virtual Lookup Lookup { get; set; }
+
+        public HttpPostedFileBase AttachmentFile { get; set; }
     }
 }
