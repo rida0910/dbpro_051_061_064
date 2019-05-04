fcd766fe8e49e2efcb14b8bbc33dc133b81d9d64
Rida Sajid <ridasajid98@gmail.com>
2019-05-04 16:03:47 -0700

    views updated

--- ProjecViews.sql
+++ ProjecViews.sql
@@ -1,27 +1,38 @@
 CREATE VIEW JobsDoneByFreelancers AS
- SELECT title, [Description] FROM Job
- Join Freelancer ON Job.CategoryID = Freelancer.CategoryId
- Join JobDone ON JobDone.JobId = Job.JobId
+ SELECT FirstName + ' ' + LastName AS [Freelancer Name], title AS [Job Title], [Description] FROM Job
+ JOIN Bid ON Job.JobId = Bid.JobID
+ JOIN Freelancer ON Bid.FreelancerID = Freelancer.FreelancerId
+ JOIN Person ON Person.PersonId = Freelancer.FreelancerId
+ where active = 0 and Accepted = 1
  
- CREATE VIEW SameEmployersDifferentProjects AS
- SELECT title, [Description] FROM Job 
-  Join Freelancer ON Job.CategoryID = Freelancer.CategoryId
- JOIN Employer ON Job.EmployerID = Employer.EmployerId
+ CREATE VIEW NoOfProjectOfFreelancerAndEmployer AS
+ SELECT E.FirstName + ' '+ E.LastName AS [Employer Name], F.FirstName + ' ' + F.LastName AS [Freelancer Name],
+ Count(BidId) AS [Number of accepted projects] FROM Person E JOIN Job ON E.PersonId = Job.EmployerID
+ JOIN Bid ON Bid.JobID = Job.JobId
+ JOIN Freelancer ON Bid.FreelancerID = Freelancer.FreelancerId
+ JOIN Person F ON F.PersonId = Freelancer.FreelancerId
+ Where Accepted = 1
+ group by E.FirstName + ' '+ E.LastName, F.FirstName + ' ' + F.LastName
 
+ 
 CREATE VIEW InactiveProjectsOfEmployers AS
 SELECT FirstName + ' ' + LastName AS EmployerName, Title , [Description] FROM Person
 JOIN Employer ON Person.PersonId = Employer.EmployerId
 JOIN Job ON Employer.EmployerId = Job.EmployerID
 JOIN Bid ON Job.JobId = Bid.JobID 
-WHERE Active = 0
+WHERE Active = 0 and Accepted = 1
 
 
  CREATE VIEW JobsPostedByEmployer AS
- SELECT Title, [Description] FROM Job 
+ SELECT FirstName + ' ' + LastName AS EmployerName, Title, MinPayment AS [Minimum Payment], MaxPayment AS [Maximum Payment], CategoryName AS [Job Category] FROM Job 
  JOIN Employer ON Job.EmployerID = Employer.EmployerId
-
+ JOIN Person ON Job.EmployerID = Person.PersonId
+ JOIN Category ON Job.CategoryID = Category.CategoryId
  
  
  CREATE VIEW ListOfFreelancers AS
- SELECT FirstName + ' ' + LastName AS Name, ProfessionalTitle, ProfessionalOverview FROM Person
+ SELECT FirstName + ' ' + LastName AS Name, ProfessionalTitle, ProfessionalOverview, Value AS Gender, Nationality, [Address] FROM Person
  JOIN Freelancer ON Person.PersonId = Freelancer.FreelancerId
+ JOIN [Lookup] ON Person.Gender = Lookup.Id
+
+
--- ViewsOfReports.sql
+++ ViewsOfReports.sql
@@ -1,31 +1,27 @@
 
 CREATE VIEW EmployersList 
 AS
-SELECT FirstName + ' ' + LastName AS EmployersName, UserName, Email, [Address],  CompanyName FROM Person 
+SELECT FirstName + ' ' + LastName AS EmployerName, Email, Value AS Gender, Nationality,  CompanyName AS Company FROM Person 
 JOIN Employer ON Person.PersonId = Employer.EmployerId
 JOIN AspNetUsers ON Person.User_AccountID = AspNetUsers.Id
-JOIN Company ON Employer.CompanyId = Company.CompanyId 
+JOIN Company ON Employer.CompanyId = Company.CompanyId
+JOIN [LookUp] ON Person.Gender = [LookUp].Id
 
-/*CREATE Procedure EmployersActiveProjects
-AS
-SELECT FirstName + ' ' + LastName AS EmployerName, Title AS JobTitle, Deadline FROM Person
-JOIN Employer ON Person.PersonId = Employer.EmployerId
-JOIN AspNetUsers ON Person.User_AccountID = AspNetUsers.Id
-JOIN Job ON Employer.EmployerId = Job.EmployerID
-WHERE JobId NOT IN (SELECT JobId FROM JobDone)*/
 
 CREATE VIEW EmployersActiveProjects
 AS
-SELECT FirstName + ' ' + LastName AS EmployerName, Title AS JobTitle, Deadline FROM Person
-JOIN Employer ON Person.PersonId = Employer.EmployerId
+SELECT E.FirstName + ' ' + E.LastName AS EmployerName, Title AS JobTitle, F.FirstName + ' ' + F.LastName AS FreelancerName , convert(varchar, Deadline, 106) AS DeadLine FROM Person AS E
+JOIN Employer ON E.PersonId = Employer.EmployerId
 JOIN Job ON Employer.EmployerId = Job.EmployerID
 JOIN Bid ON Job.JobId = Bid.JobID 
+JOIN Freelancer ON Bid.FreelancerID = Freelancer.FreelancerId
+JOIN Person AS F ON F.PersonId = Freelancer.FreelancerId
 WHERE Active = 1
 
 
 CREATE VIEW FreelancersOfAProject 
 AS
-SELECT Title AS ProjectTitle, FirstName + ' ' + LastName AS FreelancerName, UserName FROM Job
+SELECT Title AS ProjectTitle, FirstName + ' ' + LastName AS FreelancerName, Email FROM Job
 JOIN Bid ON Job.JobId = Bid.JobID 
 JOIN Freelancer ON Bid.FreelancerID = Freelancer.FreelancerId
 JOIN Person ON Freelancer.FreelancerId = Person.PersonId
@@ -42,22 +38,13 @@ WHERE Accepted = 'False' AND Active = 0
 
 CREATE VIEW FreelancerWorkedWithEmp
 AS
-SELECT UserName AS Freelancer, FirstName + ' ' + LastName AS EmployerName FROM AspNetUsers
-JOIN Person ON AspNetUsers.Id = Person.User_AccountID
-JOIN Bid ON Person.PersonId = Bid.FreelancerID
+SELECT F.FirstName + ' ' + F.LastName AS Freelancer, E.FirstName + ' ' + E.LastName AS EmployerName, Title AS JobTitle
+FROM Person F
+JOIN Freelancer ON F.PersonId = Freelancer.FreelancerId
+JOIN Bid ON Freelancer.FreelancerId = Bid.FreelancerID
 JOIN Job ON Bid.JobID = Job.JobId
 JOIN Employer ON Job.EmployerID = Employer.EmployerId
-
-
-
-
-CREATE Procedure FreelancersOfAProject @JobTitle varchar(100)
-AS
-SELECT Title AS ProjectTitle, FirstName + ' ' + LastName AS FreelancerName, UserName FROM Job
-JOIN 
-
-
-WHERE Title = @JobTitle
+JOIN Person E ON Employer.EmployerId = E.PersonId
 
 
 
@@ -70,4 +57,5 @@ WHERE Title = @JobTitle
 
 
 
-COUNT(Email) AS TotalFreelancers ,
+
+
