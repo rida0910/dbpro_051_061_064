21e49bcdfa5cb846077dab92a5d3c82c12fee9d1
Faiqa Saeed <faiqasaeed17@gmail.com>
2019-05-04 11:36:20 +0500

    report controller

--- FreelancerMarketplace/FreelancerMarketplace/Controllers/ReportController.cs
+++ FreelancerMarketplace/FreelancerMarketplace/Controllers/ReportController.cs
@@ -8,8 +8,62 @@ using System.Web.Mvc;
 
 namespace FreelancerMarketplace.Controllers
 {
+
     public class ReportController : Controller
     {
+        FreelancerMarketplace.Models.DB66Entities db = new FreelancerMarketplace.Models.DB66Entities();
+       
+        // GET: Report
+        public ActionResult Employers()
+        {
+            return ShowEmployersList();
+        }
+        public ActionResult ShowEmployersList()
+        {
+
+            //CrMVCApp.Models.Customer c;
+            var c = (from b in db.EmployersLists select b).ToList();
+
+            Employers rpt = new Employers();
+            rpt.Load();
+            rpt.SetDataSource(c);
+            Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            return File(s, "application/pdf");
+        }
+
+        public ActionResult ActiveProjects()
+        {
+            return ShowEmployersActiveProjects();
+        }
+        public ActionResult ShowEmployersActiveProjects()
+        {
+
+            //CrMVCApp.Models.Customer c;
+            var c = (from b in db.EmployersActiveProjects select b).ToList();
+
+            EmpActiveJobs rpt = new EmpActiveJobs();
+            rpt.Load();
+            rpt.SetDataSource(c);
+            Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            return File(s, "application/pdf");
+        }
+
+        public ActionResult UnAccepteJobs()
+        {
+            return ShowEmployersUnacceptedProjects();
+        }
+        public ActionResult ShowEmployersList()
+        {
+
+            //CrMVCApp.Models.Customer c;
+            var c = (from b in db.EmployersLists select b).ToList();
+
+            Employers rpt = new Employers();
+            rpt.Load();
+            rpt.SetDataSource(c);
+            Stream s = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+            return File(s, "application/pdf");
+        }
         // GET: Report
         public ActionResult Index()
         {
