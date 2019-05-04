f2ee76db31b5bf203ac5b9f592e766d3042dcb7e
AQSAAQSAKHAN <miniondora234@yahoo.com>
2019-05-04 12:15:53 +0500

    Update ReportsController.cs

--- FreelancerMarketplace/FreelancerMarketplace/Controllers/ReportsController.cs
+++ FreelancerMarketplace/FreelancerMarketplace/Controllers/ReportsController.cs
@@ -95,75 +95,7 @@ namespace FreelancerMarketplace.Controllers
 
 
         // GET: Reports/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Reports/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Reports/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reports/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Reports/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Reports/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Reports/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+       
         }
     }
 }
